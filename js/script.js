56ef2f002351fcc8fca191c31ac8dae15bf93711
franigorl <franklinigor.iff@gmail.com>
2023-04-29 22:00:32 -0300

    criados os arquivos principais do meu primeiro projeto de JavaScript puto orientado a objetos

---
+++ js/script.js
@@ -0,0 +1,21 @@
+class Produto{
+
+    constructor() {
+        this.id = 0;
+        this.nomeProduto = '';
+        this.valor = 0;
+    }
+
+    adicionar() {
+        alert()
+    }
+
+    excluir() {
+
+
+        alert("Item deletado com sucesso!")
+    }
+
+}
+
+var produto = new Produto()
